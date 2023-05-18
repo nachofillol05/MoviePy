171aed84aa3c3e27c468df158c56e47a59b7694e
FrancoECeballos <franco.ceballos2005@gmail.com>
2023-05-18 11:16:05 -0300

    Añadí los botones Cut y Set Volume

--- interfaz.py
+++ interfaz.py
@@ -17,8 +17,12 @@ class VideoEditor(QMainWindow):
         self.volume_button.setGeometry(50, 400, 100, 30)
         self.volume_button.clicked.connect(self.open_volume_dialog)
 
+        self.newaudio_button = QPushButton("Set Audio", self)
+        self.newaudio_button.setGeometry(200, 400, 100, 30)
+        self.newaudio_button.clicked.connect(self.change_audio)
+
         self.audiont_button = QPushButton("Remove Audio", self)
-        self.audiont_button.setGeometry(200, 400, 100, 30)
+        self.audiont_button.setGeometry(350, 400, 100, 30)
         self.audiont_button.clicked.connect(self.remove_audio)
                 
         self.open_button = QPushButton("Open", self)
@@ -36,6 +40,10 @@ class VideoEditor(QMainWindow):
         self.resize_button = QPushButton("Resize", self)
         self.resize_button.setGeometry(200, 260, 100, 30)
         self.resize_button.clicked.connect(self.resize)
+
+        self.cut_button = QPushButton("Cut", self)
+        self.cut_button.setGeometry(350, 260, 100, 30)
+        self.cut_button.clicked.connect(self.cut)
         
         self.rotate_button = QPushButton("Make gif", self)
         self.rotate_button.setGeometry(500, 100, 100, 30)
@@ -54,7 +62,7 @@ class VideoEditor(QMainWindow):
         self.label_archivo.setGeometry(0, 43, 500, 30)
 
         self.label_tamaño = QLabel(self)
-        self.label_tamaño.setText("Tamaño y Rotación")
+        self.label_tamaño.setText("Tamaño, Duración y Rotación")
         self.label_tamaño.setGeometry(0, 195, 500, 30)
 
         self.label_audio = QLabel(self)
@@ -62,15 +70,15 @@ class VideoEditor(QMainWindow):
         self.label_audio.setGeometry(0, 343, 500, 30)
 
         self.label_linea1 = QLabel(self)
-        self.label_linea1.setText("___________________________________________________________________________________________________________________________________________________________")
+        self.label_linea1.setText("________________________________________________________________________________________")
         self.label_linea1.setGeometry(0, 25, 650, 30)
 
         self.label_linea2 = QLabel(self)
-        self.label_linea2.setText("___________________________________________________________________________________________________________________________________________________________")
+        self.label_linea2.setText("________________________________________________________________________________________")
         self.label_linea2.setGeometry(0, 175, 650, 30)
 
         self.label_linea3 = QLabel(self)
-        self.label_linea3.setText("___________________________________________________________________________________________________________________________________________________________")
+        self.label_linea3.setText("________________________________________________________________________________________")
         self.label_linea3.setGeometry(0, 325, 650, 30)
 
         self.label_finalizado = QLabel(self)
@@ -103,15 +111,6 @@ class VideoEditor(QMainWindow):
         if file_dialog.exec_() == QFileDialog.Accepted:
             self.guardado_archivo = file_dialog.selectFile()
             self.label_ubicacion.setText(self.guardado_archivo)'''
-            
-    def add_title(self):
-        if self.video is not None:
-            title_text, ok = QInputDialog.getText(self, "Add Title", "Enter Title Text:")
-            if ok:
-                title_clip = TextClip(title_text, fontsize=70, color='white')
-                video_with_title = CompositeVideoClip([self.video, title_clip.set_pos(('center', 'top'))])
-                self.video = video_with_title
-                self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
     
     def gif(self):
         self.label_finalizado.setText("Procesando video...")
@@ -124,12 +123,16 @@ class VideoEditor(QMainWindow):
             try:
                 resultado = QInputDialog.getText(self, "Rotate", "Cuantos grados:(Numero porfavor)")#es ua tupla
                 grados = int(resultado[0])
+                self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
                 
             except ValueError:
                 print("Ingrese un numero")
             if grados != 0:
                 print("lklegueeeeeeeee")
                 self.video = self.video.rotate(grados)
+
+    def cut(self):
+        print("xd")
                 
     def save_video(self):
         self.label_finalizado.setText("Procesando el video...")
@@ -143,8 +146,9 @@ class VideoEditor(QMainWindow):
                 volume, ok = QInputDialog.getText(self, "Change Volume", "Enter New Volume:")
                 if ok:
                     self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
-                    self.video = self.video.volumex(volume)
-
+                    self.audio = AudioFileClip(self.video)
+                    self.audio2 = self.audio.afx(vfx.volumex, volume)
+                    self.video = self.video.set_audio(self.audio2)
             except ValueError:
                 print("Ingrese un numero")
                 
@@ -152,6 +156,16 @@ class VideoEditor(QMainWindow):
         if self.video is not None:
             self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
             self.video = self.video.without_audio()
+
+    def change_audio(self):
+        if self.video is not None:
+            file_dialog = QFileDialog(self)
+            file_dialog.setNameFilter("Audio Files (*.wav *.mp3)")
+            if file_dialog.exec_() == QFileDialog.Accepted:
+                self.file_path = file_dialog.selectedFiles()[0]
+                self.audio = AudioFileClip(self.file_path)
+                self.video = self.video.set_audio(self.audio)
+                self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
