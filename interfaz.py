be8b04f1c168489ace01dc136778230b136327ca
FrancoECeballos <franco.ceballos2005@gmail.com>
2023-05-18 10:09:35 -0300

    Interfaz Reworkeada

--- interfaz.py
+++ interfaz.py
@@ -7,51 +7,75 @@ class VideoEditor(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("MoviePy Video Editor")
-        self.setGeometry(100, 100, 1250, 150)
+        self.setGeometry(100, 100, 650, 520)
         
         self.save_button = QPushButton("Save", self)
-        self.save_button.setGeometry(50, 50, 100, 30)
+        self.save_button.setGeometry(50, 100, 100, 30)
         self.save_button.clicked.connect(self.save_video)
         
-        self.title_button = QPushButton("Add Title", self)
-        self.title_button.setGeometry(350, 50, 100, 30)
-        self.title_button.clicked.connect(self.add_title)
-        
         self.volume_button = QPushButton("Volume", self)
-        self.volume_button.setGeometry(950, 50, 100, 30)
+        self.volume_button.setGeometry(50, 400, 100, 30)
         self.volume_button.clicked.connect(self.open_volume_dialog)
 
         self.audiont_button = QPushButton("Remove Audio", self)
-        self.audiont_button.setGeometry(1100, 50, 100, 30)
+        self.audiont_button.setGeometry(200, 400, 100, 30)
         self.audiont_button.clicked.connect(self.remove_audio)
                 
         self.open_button = QPushButton("Open", self)
-        self.open_button.setGeometry(200, 50, 100, 30)
+        self.open_button.setGeometry(200, 100, 100, 30)
         self.open_button.clicked.connect(self.open_file_dialog)
 
         self.open_button = QPushButton("Add Clip", self)
-        self.open_button.setGeometry(800, 50, 100, 30)
+        self.open_button.setGeometry(350, 100, 100, 30)
         self.open_button.clicked.connect(self.open_clip_dialog)
+
+        self.rotate_button = QPushButton("Rotate", self)
+        self.rotate_button.setGeometry(50, 260, 100, 30)
+        self.rotate_button.clicked.connect(self.rotate)
+
+        self.resize_button = QPushButton("Resize", self)
+        self.resize_button.setGeometry(200, 260, 100, 30)
+        self.resize_button.clicked.connect(self.resize)
+        
+        self.rotate_button = QPushButton("Make gif", self)
+        self.rotate_button.setGeometry(500, 100, 100, 30)
+        self.rotate_button.clicked.connect(self.gif)
         
         self.label_ubicacion = QLabel(self)
         self.label_ubicacion.setText("Ingrese la ruta del archivo")
-        self.label_ubicacion.setGeometry(150, 20, 500, 30)
+        self.label_ubicacion.setGeometry(0, 10, 600, 30)
         
         self.label_duracion = QLabel(self)
         self.label_duracion.setText("")
-        self.label_duracion.setGeometry(200, 75, 500, 30)
+        self.label_duracion.setGeometry(0, 450, 500, 30)
         
+        self.label_archivo = QLabel(self)
+        self.label_archivo.setText("Archivo")
+        self.label_archivo.setGeometry(0, 43, 500, 30)
+
+        self.label_tamaño = QLabel(self)
+        self.label_tamaño.setText("Tamaño y Rotación")
+        self.label_tamaño.setGeometry(0, 195, 500, 30)
+
+        self.label_audio = QLabel(self)
+        self.label_audio.setText("Audio")
+        self.label_audio.setGeometry(0, 343, 500, 30)
+
+        self.label_linea1 = QLabel(self)
+        self.label_linea1.setText("___________________________________________________________________________________________________________________________________________________________")
+        self.label_linea1.setGeometry(0, 25, 650, 30)
+
+        self.label_linea2 = QLabel(self)
+        self.label_linea2.setText("___________________________________________________________________________________________________________________________________________________________")
+        self.label_linea2.setGeometry(0, 175, 650, 30)
+
+        self.label_linea3 = QLabel(self)
+        self.label_linea3.setText("___________________________________________________________________________________________________________________________________________________________")
+        self.label_linea3.setGeometry(0, 325, 650, 30)
+
         self.label_finalizado = QLabel(self)
         self.label_finalizado.setText("")
-        self.label_finalizado.setGeometry(200, 100, 500, 30)
-                    
-        self.rotate_button = QPushButton("Rotate", self)
-        self.rotate_button.setGeometry(500, 50, 100, 30)
-        self.rotate_button.clicked.connect(self.rotate)
-        
-        self.rotate_button = QPushButton("Hacer gif", self)
-        self.rotate_button.setGeometry(650, 50, 100, 30)
-        self.rotate_button.clicked.connect(self.gif)
+        self.label_finalizado.setGeometry(0, 475, 650, 30)
         
         self.video = None
     
@@ -115,11 +139,14 @@ class VideoEditor(QMainWindow):
 
     def open_volume_dialog(self):
         if self.video is not None:
-            volume, ok = QInputDialog.getText(self, "Change Volume", "Enter New Volume:")
-            if ok:
-                self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
-                self.videovolume = self.video.volumex(volume)
-                self.video = self.videovolume
+            try:
+                volume, ok = QInputDialog.getText(self, "Change Volume", "Enter New Volume:")
+                if ok:
+                    self.label_finalizado.setText("NO CIERRE LA APLICACION O LOS CAMBIOS SERAN PERDIDOS")
+                    self.video = self.video.volumex(volume)
+
+            except ValueError:
+                print("Ingrese un numero")
                 
     def remove_audio(self):
         if self.video is not None:
